280d88580182438e26806b846f266f4c142f9b74
James de Villiers <james@octoco.ltd>
2024-05-20 09:36:02 +0200

    feat: added a few small stylistic changes

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # dependency-checker
 This is Octoco's dependecy license checker. It runs through the license of all the modules used in your repository and will highlight any non permissive licenses. For more information on the different license visit [this](https://en.wikipedia.org/wiki/Comparison_of_free_and_open-source_software_licenses) link.
 
+If any of the licenses in your project are not in our list you will be prompted to continue or not. This tool is therefor better suited for local testing and not for a CI/CD pipeline.
+
 ## Installation
 The CLI tool can be installed globally using `npm install -g git+https://github.com/octoco-ltd/dependency-checker.git`.
 
--- index.js
+++ index.js
@@ -1,101 +1,101 @@
 #!/usr/bin/env node
 
-import * as checker from 'license-checker'
-import {promptForPackage} from "./prompt-for-package.js";
-import fs from 'fs'
+import * as checker from 'license-checker';
+import { promptForPackage } from './prompt-for-package.js';
+import fs from 'fs';
 
+// List of permissive licenses. If any license is not in this list prompt the user.
 const allowedLicenses = [
-    "AFL-3.0",
-    "AFL-2.1",
-    "AFL-2.0",
-    "AFL-1.2",
-    "AFL-1.1",
-    "Apache-2.0",
-    "Apache-1.1",
-    "Apache-1.0",
-    "APSL-1.0",
-    "APSL-1.1",
-    "APSL-1.2",
-    "APSL-2.0",
-    "Beerware",
-    "BSD-4-Clause",
-    "BSD-3-Clause",
-    "BSD-2-Clause",
-    "BSD-1-Clause",
-    "0BSD",
-    "CC BY",
-    "CC0",
-    "CECILL-1.0",
-    "CECILL-1.1",
-    "CECILL-2.0",
-    "CECILL-2.1",
-    "CECILL-B",
-    "CECILL-C",
-    "CDDL-1.1",
-    "CDDL-1.0",
-    "CPL-1.0", // Modification is copylefted!
-    "EPL-2.0", // Modification is copylefted!
-    "EPL-1.0", // Modification is copylefted!
-    "ECL-1.0",
-    "ECL-2.0",
-    "BSD-2-Clause",
-    "ISC",
-    "LPPL-1.0",
-    "LPPL-1.1",
-    "LPPL-1.2",
-    "LPPL-1.3a",
-    "LPPL-1.3c",
-    "MIT",
-    "MIT*",
-    "PSF-2.0",
-    "W3C-19980720",
-    "W3C-20150513",
-    "W3C",
-    "Unlicense",
-    "WTFPL",
-    "Zlib",
-    "CC-BY-3.0",
-    "CC-BY-4.0",
-    "CC0-1.0",
-    "Python-2.0"
-]
+    'AFL-3.0',
+    'AFL-2.1',
+    'AFL-2.0',
+    'AFL-1.2',
+    'AFL-1.1',
+    'Apache-2.0',
+    'Apache-1.1',
+    'Apache-1.0',
+    'APSL-1.0',
+    'APSL-1.1',
+    'APSL-1.2',
+    'APSL-2.0',
+    'Beerware',
+    'BSD-4-Clause',
+    'BSD-3-Clause',
+    'BSD-2-Clause',
+    'BSD-1-Clause',
+    '0BSD',
+    'CC BY',
+    'CC0',
+    'CECILL-1.0',
+    'CECILL-1.1',
+    'CECILL-2.0',
+    'CECILL-2.1',
+    'CECILL-B',
+    'CECILL-C',
+    'CDDL-1.1',
+    'CDDL-1.0',
+    'CPL-1.0', // Modification is copylefted!
+    'EPL-2.0', // Modification is copylefted!
+    'EPL-1.0', // Modification is copylefted!
+    'ECL-1.0',
+    'ECL-2.0',
+    'BSD-2-Clause',
+    'ISC',
+    'LPPL-1.0',
+    'LPPL-1.1',
+    'LPPL-1.2',
+    'LPPL-1.3a',
+    'LPPL-1.3c',
+    'MIT',
+    'MIT*',
+    'PSF-2.0',
+    'W3C-19980720',
+    'W3C-20150513',
+    'W3C',
+    'Unlicense',
+    'WTFPL',
+    'Zlib',
+    'CC-BY-3.0',
+    'CC-BY-4.0',
+    'CC0-1.0',
+    'Python-2.0',
+];
 
 const cliTool = async () => {
-    checker.init({ start: './' }, async function (err, packages) {
-        if (err) {
-            console.error(err)
-        } else {
-            const packageKeys = Object.keys(packages)
-            for (const key of packageKeys) {
-                const lic = packages[key].licenses
-                // We don't want to evaluate our own package.json
-                if (packages[key].path !== './') {
-                    if (!allowedLicenses.includes(lic)) {
-                        // We don't want to evaluate our own package.json
-                        const answer = await promptForPackage(key, lic)
-                        console.log('\n')
-                        if (answer !== 'Y'){
-                            throw Error(`Package ${key} (${packages[key].repository}) uses the ${lic} license which is not allowed. Path to package: ${packages[key].path}`)
+    try {
+
+        checker.init({ start: './' }, async function(err, packages) {
+            if (err) {
+                console.error(err);
+            } else {
+                const packageKeys = Object.keys(packages);
+                for (const key of packageKeys) {
+                    const lic = packages[key].licenses;
+                    // We don't want to evaluate our own package.json
+                    if (packages[key].path !== './') {
+                        if (!allowedLicenses.includes(lic)) {
+                            const answer = await promptForPackage(key, lic);
+                            console.log('\n');
+                            if (answer !== 'Y') {
+                                throw Error(`Package ${key} (${packages[key].repository}) uses the ${lic} license which is not allowed. Path to package: ${packages[key].path}`);
+                            }
                         }
                     }
                 }
-
+                console.info('All license where checked and are allowed!');
             }
-            console.log('All license where checked and are allowed!')
+        });
+
+        console.info('Checking for LICENSE file in repo');
+        if (!fs.existsSync('./LICENSE')) {
+            fs.appendFile('LICENSE', 'Copyright (c) 2024 Octoco Consulting (Pty) Ltd. All rights reserved.', function(err) {
+                if (err) {
+                    throw err;
+                }
+                console.info('LICENSE file created!');
+            });
         }
-    })
-
-    console.info('Checking for LICENSE file in repo')
-    if (!fs.existsSync('./LICENSE')) {
-        fs.appendFile('LICENSE', 'Copyright (c) 2024 Octoco Consulting (Pty) Ltd. All rights reserved.', function (err) {
-            if (err) {
-                console.error(err)
-                throw err;
-            }
-            console.info('LICENSE file created!');
-        })
+    } catch (err) {
+        console.error(err);
     }
-
-}
-
-cliTool()
+};
